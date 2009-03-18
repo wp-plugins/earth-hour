de4d22d411eca3a92c781106ec2ad8b5eab4ba45
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-18 17:36:04 +0000

    Earth hour
    git-svn-id: https://plugins.svn.wordpress.org/earth-hour/trunk@103227 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ earth-hour.php
@@ -0,0 +1,7 @@
+<?php
+Plugin Name: Earth Hour
+Plugin URI: http://www.bravenewcode.com/earth-hour/
+Description: Changing the world, one website at a time.
+Author: Dale Mugford and Duane STorey
+Version: 1.0.0
+Author URI: http://www.bravenewcode
