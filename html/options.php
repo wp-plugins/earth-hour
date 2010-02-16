952d7a84b6a90f5df64f050b9e7e338be5c8d75a
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 19:54:06 +0000

    more admin refinements
    git-svn-id: https://plugins.svn.wordpress.org/earth-hour/trunk@206675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/options.php
+++ html/options.php
@@ -57,4 +57,53 @@
 	</div><!-- postbox -->
 </div><!-- earth-hour-head -->
 
+<!-- Earth Hour Settings Pane -->
+<div class="metabox-holder">
+	<div class="postbox">
+		<h3><span class="global-settings">&nbsp;</span><?php _e( "General Settings", "earth-hour" ); ?></h3>
+
+			<div class="left-content">
+				<h4><?php _e( "Banner Placement", "earth-hour" ); ?></h4>
+				<p><?php _e( "Before Earth Hour begins, your site shows a banner in a fixed position showing your support for the event.", "earth-hour" ); ?></p>
+				<p><?php _e( "Choose whether the banner is shown at the top or bottom of your website.", "earth-hour" ); ?></p>
+
+				<br /><br />
+
+				<h4><?php _e( "Earth Hour Image", "earth-hour" ); ?></h4>
+				<p><?php _e( "During Earth Hour all links to your site will show this image and the text you choose in the option below.", "earth-hour" ); ?></p>
+				<p><?php _e( "Choose between the two provided images, or link to one of your own.", "earth-hour" ); ?></p>
+				<p><?php _e( "Our images are 350px x 350px, and we'd recommend you keep yours at a similar size to work correctly.", "earth-hour" ); ?></p>
+				
+				<br /><br />
+
+				<h4><?php _e( "Earth Hour Text", "earth-hour" ); ?></h4>
+				<p><?php _e( "Along with the image above, during Earth Hour your site will display a message to all your visitors. You can edit that message here.", "earth-hour" ); ?></p>
+
+				<br /><br />
+
+				<h4><?php _e( "Preview Site", "earth-hour" ); ?></h4>
+				<p><?php _e( "Clicking the Preview button will show you what your site will look like during Earth Hour.", "earth-hour" ); ?></p>
+				<p><?php _e( "Only you will see the preview, not your visitors.", "earth-hour" ); ?></p>
+			</div>
+
+			<div class="right-content">
+
+			</div>
+			
+	<div class="bnc-clearer"></div>
+	</div><!-- postbox -->
+</div><!-- metabox -->
+
+	<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+		<input type="submit" name="submit" value="<?php _e('Save Options', 'earth-hour' ); ?>" id="bnc-button" class="button-primary" />
+	</form>
+	
+	<form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+		<input type="submit" onclick="return confirm('<?php _e('Restore default Earth Hour settings?', 'earth-hour' ); ?>');" name="reset" value="<?php _e('Restore Defaults', 'earth-hour' ); ?>" id="bnc-button-reset" class="button-highlighted" />
+	</form>
+		
+		<?php echo('' . Earth_Hour('<div class="bnc-plugin-version"> This is ','</div>') . ''); ?>
+
+	<div class="bnc-clearer"></div>
+
 </div><!-- global earth-hour -->
