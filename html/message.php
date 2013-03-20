218da66a6bac0556f3d8467a20b30a054e8e0d1d
bravenewcode <bravenewcode@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-20 20:53:42 +0000

    More changes for earth hour
    git-svn-id: https://plugins.svn.wordpress.org/earth-hour/trunk@684916 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- html/message.php
+++ html/message.php
@@ -16,7 +16,7 @@
 		<?php if ( isset( $_GET['earth_hour_preview'] )	) { ?><div class="preview"><span><?php _e( "preview mode", "earth-hour" ); ?></span> <a href="<?php bloginfo('wpurl'); ?>/wp-admin/options-general.php?page=earth-hour.php" id="admin-return">&larr; <?php _e( "Earth Hour Admin Settings", "earth-hour" ); ?></a></div><?php } ?>
 			<div id="earth_hour">
 				<p class="admin-text"><?php echo $settings['earth_hour_text']; ?></p>
-				<p class="below-image"><?php echo sprintf( __ngettext( "There is currently %d other WordPress site participating in the event.",  "There are  %d other WordPress sites participating in the event.", $count, "earth-hour"), $count); ?></p>
+				<!-- <p class="below-image"><?php echo sprintf( __ngettext( "There is currently %d other WordPress site participating in the event.",  "There are  %d other WordPress sites participating in the event.", $count, "earth-hour"), $count); ?></p> -->
 				<p><?php echo __( "If you'd like to join us, download the <br /><a href=\"http://www.bravenewcode.com/free-stuff/\">WordPress Earth Hour plugin</a>", "earth-hour" ) ?></p>
 				<p><?php echo sprintf( __ngettext( "This website will be back online in %d minute.", "This website will be back online in %d minutes.", $earth_hour_minutes, "earth-hour" ), $earth_hour_minutes ); ?></p>
 			</div>
